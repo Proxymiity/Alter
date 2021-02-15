aae4645a13a00c4dbc8d2b0c27763b0fafb6c0bc
Proxymiity <commits@proxymiity.fr>
2021-02-15 20:31:34 +0100

    Added idban and kick commands

--- plugins/moderation.py
+++ plugins/moderation.py
@@ -22,12 +22,60 @@ class Moderation(commands.Cog):
         else:
             reason = loc["ban_reason_empty"].format(ctx.author.name)
         member = ctx.guild.get_member(user.id)
+        if member is None:
+            await ctx.send(loc["user_notfound"])
+            return
         try:
             await member.send(loc["ban_dm"].format(ctx.guild.name, reason))
         except discord.Forbidden:
-            pass
+            await ctx.send(loc["dm_error"])
         await member.ban(reason=reason)
         await ctx.send(loc["ban_success"].format(member.name, reason))
+
+    @checks.permission(discord.Permissions.kick_members)
+    @commands.command(help="kick_help", brief="kick_brief")
+    async def kick(self, ctx, user: discord.User, *, reason=None):
+        if user == ctx.me or user == ctx.author or user == ctx.guild.owner:
+            await ctx.send(loc["target_error"])
+            return
+        if reason:
+            reason = loc["kick_reason"].format(ctx.author.name, reason)
+        else:
+            reason = loc["kick_reason_empty"].format(ctx.author.name)
+        member = ctx.guild.get_member(user.id)
+        if member is None:
+            await ctx.send(loc["user_notfound"])
+            return
+        try:
+            await member.send(loc["kick_dm"].format(ctx.guild.name, reason))
+        except discord.Forbidden:
+            await ctx.send(loc["dm_error"])
+        await member.kick(reason=reason)
+        await ctx.send(loc["kick_success"].format(member.name, reason))
+
+    @checks.permission(discord.Permissions.kick_members)
+    @commands.command(help="idban_help", brief="idban_brief")
+    async def idban(self, ctx, user: int, *, reason=None):
+        try:
+            member = ctx.guild.get_member(user)
+        except discord.NotFound:
+            member = None
+        if member:
+            await ctx.send(loc["idban_is_member"].format(member.name, ctx.prefix, member.id))
+            return
+        if reason:
+            reason = loc["ban_reason"].format(ctx.author.name, reason)
+        else:
+            reason = loc["ban_reason_empty"].format(ctx.author.name)
+        try:
+            await self.bot.http.ban(user, ctx.guild.id, reason=reason)
+        except discord.errors.HTTPException or discord.errors.NotFound:
+            await ctx.send(loc["user_notfound"])
+        else:
+            for x in await ctx.guild.bans():
+                if x.user.id == user:
+                    member = x.user
+            await ctx.send(loc["ban_success"].format(member.name, reason))
 
 
 def setup(bot):
