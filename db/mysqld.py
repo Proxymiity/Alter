a48d18418810763e6673c5bd42d4251f0f799bde
Proxymiity <commits@proxymiity.fr>
2021-02-19 13:46:52 +0100

    fixed injections and a reading bug

--- db/mysqld.py
+++ db/mysqld.py
@@ -7,33 +7,43 @@ db = mysql.connector.connect(host=db_cfg["host"],
 dbc = db.cursor()
 
 
+def sanitize(val):
+    return "".join(x for x in val if x.isalnum())
+
+
 def create_table(name: str):
-    dbc.execute("CREATE TABLE IF NOT EXISTS %s(sid INTEGER, name TEXT, value TEXT)", name)
+    name = sanitize(name)
+    dbc.execute("CREATE TABLE IF NOT EXISTS {}(sid INTEGER, name TEXT, value TEXT)".format(name))
 
 
 def delete_table(name: str):
+    name = sanitize(name)
     dbc.execute("SET foreign_key_checks = 0")
-    dbc.execute("DROP TABLE IF EXISTS %s", name)
+    dbc.execute("DROP TABLE IF EXISTS {}".format(name))
     dbc.execute("SET foreign_key_checks = 1")
 
 
 def read(table: str, sid: int, name: str):
+    table = sanitize(table)
     try:
-        dbc.execute("SELECT value FROM %s WHERE sid=%s AND name=%s", (table, sid, name))
+        dbc.execute("SELECT value FROM {} WHERE sid=%s AND name=%s".format(table), (sid, name))
         value = dbc.fetchall()[0][0]
     except IndexError:
         value = None
+    db.commit()
     return value
 
 
 def write(table: str, sid: int, name: str, value: str):
+    table = sanitize(table)
     if read(table, sid, name) is None:
-        dbc.execute("INSERT INTO %s(sid, name, value) VALUES (%s, %s, %s)", (table, sid, name, value))
+        dbc.execute("INSERT INTO {}(sid, name, value) VALUES (%s, %s, %s)".format(table), (sid, name, value))
     else:
-        dbc.execute("UPDATE %s SET value=%s WHERE sid=%s AND name=%s", (table, value, id, name))
+        dbc.execute("UPDATE {} SET value=%s WHERE sid=%s AND name=%s".format(table), (value, sid, name))
     db.commit()
 
 
 def delete(table: str, sid: int, name: str):
-    dbc.execute("DELETE FROM %s WHERE sid=%s AND name=%s", (table, sid, name))
+    table = sanitize(table)
+    dbc.execute("DELETE FROM {} WHERE sid=%s AND name=%s".format(table), (sid, name))
     db.commit()
