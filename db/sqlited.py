451c7817012d0aec3898ac88f9a5b6accd3d7a58
Proxymiity <commits@proxymiity.fr>
2021-02-19 11:47:26 +0100

    Fixed a query bug and a security vulnerability

--- db/sqlited.py
+++ db/sqlited.py
@@ -7,16 +7,16 @@ dbc = db.cursor()
 
 
 def create_table(name: str):
-    dbc.execute("CREATE TABLE IF NOT EXISTS {}(sid INTEGER, name TEXT, value TEXT)".format(name))
+    dbc.execute("CREATE TABLE IF NOT EXISTS %s(sid INTEGER, name TEXT, value TEXT)", name)
 
 
 def delete_table(name: str):
-    dbc.execute("DROP TABLE IF EXISTS {}".format(name))
+    dbc.execute("DROP TABLE IF EXISTS %s", name)
 
 
 def read(table: str, sid: int, name: str):
     try:
-        dbc.execute("SELECT value FROM {} WHERE sid={} AND name='{}'".format(table, sid, name))
+        dbc.execute("SELECT value FROM %s WHERE sid=%s AND name=%s", (table, sid, name))
         value = dbc.fetchall()[0][0]
     except IndexError:
         value = None
@@ -25,12 +25,12 @@ def read(table: str, sid: int, name: str):
 
 def write(table: str, sid: int, name: str, value: str):
     if read(table, sid, name) is None:
-        dbc.execute("INSERT INTO {}(sid, name, value) VALUES ({}, '{}', '{}')".format(table, sid, name, value))
+        dbc.execute("INSERT INTO %s(sid, name, value) VALUES (%s, %s, %s)", (table, sid, name, value))
     else:
-        dbc.execute("UPDATE {} SET value='{}' WHERE sid={} AND name='{}'")
+        dbc.execute("UPDATE %s SET value=%s WHERE sid=%s AND name=%s", (table, value, id, name))
     db.commit()
 
 
 def delete(table: str, sid: int, name: str):
-    dbc.execute("DELETE FROM {} WHERE sid={} AND name='{}'".format(table, sid, name))
+    dbc.execute("DELETE FROM %s WHERE sid=%s AND name=%s", (table, sid, name))
     db.commit()
