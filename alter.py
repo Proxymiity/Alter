04df4555419337bb7d6cfd35579348edcac46a92
Proxymiity <commits@proxymiity.fr>
2021-02-20 23:39:46 +0100

    Added localization for error messages

--- alter.py
+++ alter.py
@@ -3,10 +3,12 @@ from discord.ext import commands
 from utils.dataIO import dataIO
 from utils import locale as loc
 from utils import help
+from importlib import import_module
 
 config = dataIO.load_json("data/config.json")
 mn = "alter"
 dl = config["locale"]
+db = import_module(config["storage"])
 token = config["token"]
 prefix = config["prefix"]
 bot = commands.Bot(prefix, intents=discord.Intents.all())
@@ -26,24 +28,24 @@ async def on_command_error(ctx, error):
     if isinstance(error, commands.NoPrivateMessage):
         await ctx.send(loc.load(dl, mn, "err_pm"))
     elif isinstance(error, commands.PrivateMessageOnly):
-        await ctx.send(loc.load(dl, mn, "err_pm_only"))
+        await ctx.send(loc.get(ctx, db, mn, "err_pm_only"))
     elif isinstance(error, commands.DisabledCommand):
-        await ctx.send(loc.load(dl, mn, "err_disabled"))
+        await ctx.send(loc.get(ctx, db, mn, "err_disabled"))
     elif isinstance(error, commands.CommandOnCooldown):
-        await ctx.send(loc.load(dl, mn, "err_cd").format(error.retry_after))
+        await ctx.send(loc.get(ctx, db, mn, "err_cd").format(error.retry_after))
     elif isinstance(error, commands.MissingRequiredArgument):
         await help.send_cmd_help(ctx, ctx.command, error=True)
     elif isinstance(error, commands.BadArgument):
         await help.send_cmd_help(ctx, ctx.command, error=True)
     elif isinstance(error, commands.CommandInvokeError):
         if isinstance(error.original, discord.Forbidden):
-            await ctx.send(loc.load(dl, mn, "err_missing_perm"))
+            await ctx.send(loc.get(ctx, db, mn, "err_missing_perm"))
         elif "Missing Permission" in "{}".format(error):
-            await ctx.send(loc.load(dl, mn, "err_missing_perm"))
+            await ctx.send(loc.get(ctx, db, mn, "err_missing_perm"))
         elif "FORBIDDEN" in "{}".format(error):
-            await ctx.send(loc.load(dl, mn, "err_missing_perm"))
+            await ctx.send(loc.get(ctx, db, mn, "err_missing_perm"))
         else:
-            await ctx.send(loc.load(dl, mn, "err_exec").format(error))
+            await ctx.send(loc.get(ctx, db, mn, "err_exec").format(error))
     elif isinstance(error, commands.CommandNotFound):
         pass
     elif isinstance(error, commands.CheckFailure):
@@ -51,7 +53,7 @@ async def on_command_error(ctx, error):
     else:
         print("Uncaught exception {}".format(error))
         dataIO.save_json("error.json", [error])
-        await ctx.send(loc.load(dl, mn, "err_uncaught"))
+        await ctx.send(loc.get(ctx, db, mn, "err_uncaught"))
 
 
 print(loc.load(dl, mn, "logging_in").format(token[0:5]))
