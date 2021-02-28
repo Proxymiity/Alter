c752a7fffd501e2bf9d146e5fef755b05694cad7
Proxymiity <commits@proxymiity.fr>
2021-02-28 01:49:54 +0100

    Added a temporary database wiped on start

--- utils/defaults.py
+++ utils/defaults.py
@@ -3,11 +3,14 @@ from importlib import import_module
 config = dataIO.load_json("data/config.json")
 db = import_module(config["storage"])
 db_d = {"name": "Alter", "prefix": "::", "locale": "en_US"}
+tb_d = ["serversettings", "settings"]
 
 
 def check():
-    db.create_table("serversettings")
-    db.create_table("settings")
+    for t in tb_d:
+        db.create_table(t)
+    db.delete_table("temp")
+    db.create_table("temp")
     for x in db_d:
         if not db.read("settings", 0, x):
             db.write("settings", 0, x, db_d[x])
