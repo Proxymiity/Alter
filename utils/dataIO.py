7140f02ef291d125191ac6a493c637905c4b88cb
Proxymiity <commits@proxymiity.fr>
2021-04-20 23:01:11 +0200

    Changed the JSON structure and the corresponding dataIO line

--- utils/dataIO.py
+++ utils/dataIO.py
@@ -36,8 +36,7 @@ class DataIO:
 
     def _save_json(self, filename, data):
         with open(filename, encoding='utf-8', mode="w") as f:
-            json.dump(data, f, indent=4, sort_keys=True,
-                      separators=(',', ' : '))
+            json.dump(data, f, indent=4, separators=(',', ': '))
         return data
 
     def export_json(self, data):
